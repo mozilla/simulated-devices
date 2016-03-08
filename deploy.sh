adbc10ad61403e7457d9f9af3e01e99116b48959
J. Ryan Stinnett <jryans@gmail.com>
2016-03-08 14:30:38 -0600

    Give file public-read ACL

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws --profile code.cdn.mozilla.net s3 cp --cache-control max-age=86400 --content-type application/json devices.json s3://code-origin-cdn-mozilla-net/devices/devices.json
+aws --profile code.cdn.mozilla.net s3 cp --acl public-read --cache-control max-age=86400 --content-type application/json devices.json s3://code-origin-cdn-mozilla-net/devices/devices.json
